484de1cbdbf134f2072bac049616df6ac352dffc
르네상스맨 <whdtmek@gmail.com>
2022-01-11 17:11:12 +0900

    Creating the Basic Project Setup

---
+++ screens/shop/ProductDetails.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import { StyleSheet, Text, View } from 'react-native'
+
+const ProductDetails = () => {
+  return (
+    <View>
+      <Text></Text>
+    </View>
+  )
+}
+
+export default ProductDetails
+
+const styles = StyleSheet.create({})
---
+++ screens/shop/ProductOverview.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import { StyleSheet, Text, View } from 'react-native'
+
+const ProductOverview = () => {
+  return (
+    <View>
+      <Text></Text>
+    </View>
+  )
+}
+
+export default ProductOverview
+
+const styles = StyleSheet.create({})
